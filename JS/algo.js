d75bf0acd415b6f3ee4e5bffd640c5e7d4a3ec70
VANKOCKO <van.kocko@gmail.com>
2018-04-06 03:22:08 +0200

    test

---
+++ JS/algo.js
@@ -0,0 +1,33 @@
+
+ 
+ 
+/**
+  * Function :
+  * @param n integer
+  */
+ 
+function factoriel(n){
+           
+        var i = 0;
+        var fact=1;
+        while(i<n){
+            i=i+1;
+            fact=fact*i;
+            console.log(n+"!="+fact);
+        
+}
+
+ }
+
+ 
+factoriel(4);
+
+/**
+ *  Algoritme de tri
+ */
+
+function permuter(tab, i,j){
+        
+
+        
+ }

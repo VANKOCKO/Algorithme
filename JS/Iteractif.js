2254a30c3b2b3b46aba666c82cb256d3738d8498
VANKOCKO <van.kocko@gmail.com>
2018-04-07 10:23:31 +0200

    test

--- JS/Iteractif.js
+++ JS/Iteractif.js
@@ -81,7 +81,9 @@
       /**
       *  Exemple la division entiere
       *              
-      *          pour tout nombre a>=0,b>0, a/b => a=Quotient * b + rest  avec rest>= 0 et rest<b
+      *          pour tout nombre 
+      *     {Antecedent: a>=0,b>0} 
+      *     {Consequent : a=Quotient * b + rest  avec rest>= 0 et rest<b }
       */  
 
      function DivisionEntiere(a,b)
@@ -93,9 +95,83 @@
             quotient = quotient + 1;
             // { a = quotient * b + rest - b  }
             rest = rest - b;
-            console.log(a+"/"+b+"="+quotient);
+            return quotient;
          }
      }
 
+     var a=1;
+     var b=2;
+     var resultDiv= DivisionEntiere(a,b);
+     console.log(a+"/"+b+"="+resultDiv);
 
-     DivisionEntiere(4,2);
+      /**
+      *  Exemple du plus commun diviseur 
+      *              
+      *     { Antecedent : a>0 et b<0 }
+      *     {Consequent : pgcd(a,b)=pgcd(a-b,b)  et a>b 
+      *                            =pgcd(a,b-a)   et a<b }
+      * 
+      * 
+      *   debut 
+      *    
+      *      fonction pgcd(donnees a,b:naturel) : naturel
+      * 
+      *          tantque a!=b faire 
+      *                {   pgcd(a,b) = pgcd(a-b,b) et a>b 
+      *                            = pgcd(a,b-a) et a<b }
+      *               si a>b alors 
+      *
+      *                   { pgcd(a,b) = pgcd(a-b,b) et a>b  }
+      *                       a <- a - b                                 
+      *               sinon 
+      *                       b <- b - a
+      *                 {pgcd(a,b) = pgcd(b-a,b) et b>a}
+      *                 
+      *                rendre a  
+      *          fintanque 
+      *           
+      *             { a=b pgcd(a,b)   }
+      *      finfonction  
+      * 
+      *   fin
+      */  
+
+      function pgcd(a,b)
+      {
+          var a,b;
+          while(a != b)
+          {
+              if(a>b)
+              {
+                  a = a-b;
+                  return a; 
+              }
+              else
+              { 
+                  b = b-a;
+                return b;
+              } 
+          }
+      }
+
+     var res= pgcd(2,4);
+     console.log(res);
+
+
+     /**
+      * Exemple sur la multiplication 
+      * 
+      * {Antécédent : x = alpha, y = omega}
+      * {Conséquent : produit = x * y = alpha * omega}
+      * 
+      *  debut 
+      * 
+      *      fonction multiplication(Donnee x,y : naturel) :naturel
+      *      
+      *           variable p : naturel 
+      *           p <- 0
+      *           {alpha }
+      * 
+      * 
+      *  fin 
+      */
---
+++ JS/tableau.js
@@ -0,0 +1,28 @@
+/**
+ *      Auteur : van kocko 
+ *      Date: 06-04-2018  
+ */
+
+
+                /**
+                 *    Theorie 
+                 */
+
+/**
+ * Chapitre :   tableau
+ * 
+ * 
+ * 
+ * Algorithme de rechercher lineaire 
+ * 
+ * 
+ *    {Antecedent : x entier a rechercher dans le tableau tab }
+ *    {Consequent : renvoie x appartient a tab }
+ *    
+ *    debut 
+ *      
+ *        fonction rechercheTab(donnee x : naturel) : naturel
+ *             
+ *    fin
+ */
+

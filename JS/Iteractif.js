c527006d2b3d0a1d777ea42501977e8fde7b13ef
VANKOCKO <van.kocko@gmail.com>
2018-04-10 09:17:13 +0200

    test

--- JS/Iteractif.js
+++ JS/Iteractif.js
@@ -89,17 +89,17 @@
      function DivisionEntiere(a,b)
      {
          var rest=a;
-         var quotient;
+         var quotient=0;
          while(rest >= b)
          {        
             quotient = quotient + 1;
             // { a = quotient * b + rest - b  }
             rest = rest - b;
-            return quotient;
          }
+         return quotient;
      }
 
-     var a=1;
+     var a=4;
      var b=2;
      var resultDiv= DivisionEntiere(a,b);
      console.log(a+"/"+b+"="+resultDiv);
@@ -161,17 +161,102 @@
      /**
       * Exemple sur la multiplication 
       * 
-      * {Antécédent : x = alpha, y = omega}
-      * {Conséquent : produit = x * y = alpha * omega}
+      * {Antécédent : x = alpha, y = beta}
+      * {Conséquent : produit = x * y = alpha * beta}
       * 
       *  debut 
       * 
       *      fonction multiplication(Donnee x,y : naturel) :naturel
-      *      
       *           variable p : naturel 
       *           p <- 0
-      *           {alpha }
-      * 
-      * 
+      *           {alpha  * beta = p + x * y}
+      *           tantque y > 0 faire
+      *               {alpha  * beta = p + x * y  et y > 0 }
+      *               tantque y est paire faire  
+      *                   {alpha  * beta = p + x * y  et y = (y/2) * 2 > 0 }
+      *                   {alpha  * beta = p + 2x * (y/2)  et y = (y/2) * 2 > 0 }
+      *                   y<-y/2
+      *                   {alpha  * beta = p + 2x * y  et y = (y/2) * 2 > 0 } 
+      *                   {alpha  * beta = p + x * y }
+      *                   x<-2*x
+      *                   {alpha  * beta = p + x * y}
+      *               fintantque 
+      *               {alpha  * beta = p + (x-1) * y + y  et y > 0 et y impair }
+      *               p<-p+x
+      *               {alpha  * beta = p + x * y-1  et y impair }
+      *               y<-y-1
+      *               {alpha  * beta = p + x * y}
+      *           fintantque
+      *           {y = 0 et alpha * beta = p + x * y = p }
+      *           rendre p 
+      *      finfonction {multiplication}
       *  fin 
       */
+
+
+       function multiplication(x,y) {
+
+             var p = 0;
+
+             while(y>0)
+             {
+                 while(y % 2 == 0)
+                 {
+                     y = y /2;
+                     x= 2 * x;                      
+                 }
+                 p=p+x;
+                 y=y-1;
+             }
+             return p;           
+       }
+
+      var ResMulti=multiplication(2,4);
+      console.log(ResMulti);
+
+ /**
+      * Exemple sur les puissances 
+      * 
+      * {Antécédent : x = alpha, y = beta}
+      * {Conséquent : puissance = x^{y} = alpha^{beta}}
+      *   
+      * debut 
+      * 
+      *        fonction puissance(donnee: x, y : naturel) : naturel
+      *           
+      *            p<-1
+      *            {alpha^{beta} = p * x^{y} }
+      *            tantque y > 0 faire 
+      *                {alpha^{beta} = p * x^{y}}
+      *         
+      *        finfonction
+      * fin
+      * 
+      **/
+     
+      function puissance(x,y) 
+      {
+             var p=1;
+             while(y>0)
+             {
+                while(y % 2 == 0)
+                {
+                    y=y/2;
+                    x=x*x;
+                }
+                p=p*x;
+                y=y-1;
+             }
+             return p;
+      }
+     var ResulPuiss=puissance(2,3);
+     console.log(ResulPuiss); 
+
+
+
+                  /**
+                 *   Exercice corrigee 
+                 */
+
+
+                 
